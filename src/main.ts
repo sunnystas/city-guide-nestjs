f923c7fce36ca1edca74df40ddadce2d2b2eee3c
StanislavRomanov <sunnystas@gmail.com>
2020-03-05 21:59:51 +0200

    Host fix for Heroku

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('APP_PORT') || 3000);
+  await app.listen(configService.get('APP_PORT') || 3000, '0.0.0.0');
 }
 bootstrap();
