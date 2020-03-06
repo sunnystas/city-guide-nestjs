1ad4680d63ea6c6cbe6ef7215cbe74e0df2fc5b5
StanislavRomanov <sunnystas@gmail.com>
2020-03-06 14:47:12 +0200

    Integrated OpenAPI (Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('API reference')
+    .setDescription(
+      `API reference for the ${configService.get('APP_NAME')} Application`,
+    )
+    .setVersion(configService.get('APP_VERSION'))
+    .addTag(configService.get('APP_NAME'))
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(
     configService.get('PORT') || 3000,
     (configService.get('HOST') as string) || '0.0.0.0',
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,22 +4,46 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 import { RolesGuard } from './roles.guard';
 import { AuthService } from './auth.service';
 import { Roles } from './roles.decorator';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiUnauthorizedResponse,
+  ApiInternalServerErrorResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string' },
+        password: { type: 'string' },
+      },
+    },
+  })
+  @ApiCreatedResponse({ description: 'The user has successfully logged in' })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized: invalid credentials provided',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+  })
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
   // @todo just for testing purposes - will remove later
-  @Roles('admin')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Post('test')
-  async test(@Request() req) {
-    return 'ok';
-  }
+  // @Roles('user')
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Get('test')
+  // async test(@Request() req) {
+  //   return 'ok';
+  // }
 }
